549751f4660fce6e78abc3674c0538dc6fc14807
fatemeh salarian <89831604+MaryamEsmaily@users.noreply.github.com>
2023-10-02 23:05:40 +0330

    add validation for requests

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
 }
 export class GetTaskFilterDto {
